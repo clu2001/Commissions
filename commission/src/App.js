348378c138e281b75eeb6dbb0e63c355ec83c71d
Calla Lu <46143943+clu2001@users.noreply.github.com>
2020-08-28 15:54:27 -0400

    cursor hides when mouse leaves screen

--- commission/src/App.js
+++ commission/src/App.js
@@ -6,6 +6,7 @@ function App() {
 
   const Cursor = () => {
     const [position, setPosition] = useState({x:0, y:0}); 
+    const [hidden, setHidden] = useState(false); 
 
     useEffect(() => {
       addEventListeners(); 
@@ -14,17 +15,36 @@ function App() {
 
     const addEventListeners = () => {
       document.addEventListener("mousemove", onmousemove); 
+      document.addEventListener("mouseenter", onmouseenter); 
+      document.addEventListener("mouseleave", onmouseleave); 
     }; 
 
     const removeEventListeners = () => {
       document.removeEventListener("mousemove", onmousemove);
+      document.addEventListener("mouseenter", onmouseenter); 
+      document.addEventListener("mouseleave", onmouseleave); 
     }; 
 
     const onmousemove = (e) => {
       setPosition({x: e.clientX, y: e.clientY}); 
     }; 
 
-    return <div className="cursor" 
+    const onmouseenter = () => {
+      setHidden(false); 
+    }
+
+    const onmouseleave = () => {
+      setHidden(true); 
+    }
+
+    const cursorClasses = classNames(
+      'cursor',
+      {
+        'cursor--hidden': hidden
+      }
+    )
+
+    return <div className={cursorClasses} 
             style={{
                 left: `${position.x}px`, 
                 top: `${position.y}px`
