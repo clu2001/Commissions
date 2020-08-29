a2f89ffce3d3d4edf06fb0bc42cf644ab129135c
Calla Lu <46143943+clu2001@users.noreply.github.com>
2020-08-28 22:02:47 -0400

    cursor finished + made sure wouldn't load on mobile

--- commission/src/App.js
+++ commission/src/App.js
@@ -4,16 +4,33 @@ import classNames from "classnames";
 
 function App() {
 
+  // make sure that the user isn't on mobile 
+  const Mobile = () => {
+    const ua = navigator.userAgent; 
+    return /Android|Mobi/i.test(ua);
+  }; 
+
   const Cursor = () => {
 
     const [position, setPosition] = useState({x:0, y:0}); 
     const [hidden, setHidden] = useState(false);
     const [clicked, setClicked] = useState(false);  
+    const [linkHovered, setLinkHovered] = useState(false); 
 
     useEffect(() => {
       addEventListeners(); 
+      handleLinkHoverEvents(); 
       return () => removeEventListeners(); 
     }, []); 
+
+    if (typeof navigator !== 'undefined' && Mobile()) return null;
+
+    const handleLinkHoverEvents = () => {
+      document.querySelectorAll("a").forEach(el => {
+        el.addEventListener("mouseover", () => setLinkHovered(true)); 
+        el.addEventListener("mouseout", () => setLinkHovered(false)); 
+      }); 
+    };
 
     const addEventListeners = () => {
       document.addEventListener("mousemove", onmousemove); 
@@ -59,7 +76,8 @@ function App() {
       'cursor',
       {
         'cursor--clicked': clicked, 
-        'cursor--hidden': hidden
+        'cursor--hidden': hidden, 
+        'cursor--link-hovered': linkHovered
       }
     )
 
